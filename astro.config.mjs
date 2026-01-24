2a364eb4ac00c04a869a85ee905bc853b5f33d29
macaroni10y <56068155+macaroni10y@users.noreply.github.com>
2026-01-24 14:06:35 +0900

    set site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com/todo",
+  site: "https://macaroni10y.vercel.app",
   i18n: {
     defaultLocale: "ja",
     locales: ["ja", "en"],
