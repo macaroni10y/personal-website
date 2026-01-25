c92cc37e34f5d5ae49ac005171eafd54cee5da7f
macaroni10y <56068155+macaroni10y@users.noreply.github.com>
2026-01-25 13:13:24 +0900

    cloudflare settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://macaroni10y.vercel.app",
+  site: "https://macaroni10y.com",
   i18n: {
     defaultLocale: "ja",
     locales: ["ja", "en"],
